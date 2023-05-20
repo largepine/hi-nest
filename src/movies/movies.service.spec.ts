88f89889653b2f2691263c4fe2bae81be1d08e7b
Hansol <losnah24@gmail.com>
2023-05-20 16:49:35 +0900

    test function getOne error

--- src/movies/movies.service.spec.ts
+++ src/movies/movies.service.spec.ts
@@ -1,5 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { MoviesService } from './movies.service';
+import { NotFoundException } from '@nestjs/common';
 
 describe('MoviesService', () => {
   let service: MoviesService;
@@ -33,6 +34,16 @@ describe('MoviesService', () => {
       const movie = service.getOne(1);
       expect(movie).toBeDefined();
       expect(movie.id).toEqual(1);
+    });
+
+    it('should throw 404 error', () => {
+      try {
+        service.getOne(999);
+      } catch (e) {
+        expect(e).toBeInstanceOf(NotFoundException);
+        expect(e.status).toEqual(404);
+        expect(e.message).toEqual('Movie with ID : 999 not found.');
+      }
     });
   });
 });
