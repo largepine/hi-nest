055f1c0c468ceffd8803058e22e7eff9ba061510
Hansol <losnah24@gmail.com>
2023-05-22 15:08:22 +0900

    testing PATCH and DELETE movies id

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -34,11 +34,18 @@ describe('AppController (e2e)', () => {
       return request(app.getHttpServer()).get('/movies').expect(200).expect([]);
     });
 
-    it('POST', () => {
+    it('POST 201', () => {
       return request(app.getHttpServer())
         .post('/movies')
         .send({ title: 'Test', year: 2000, genres: ['test'] })
         .expect(201);
+    });
+
+    it('POST 400', () => {
+      return request(app.getHttpServer())
+        .post('/movies')
+        .send({ title: 'Test', year: 2000, genres: ['test'], other: 'thing' })
+        .expect(400);
     });
 
     it('DELETE', () => {
@@ -50,7 +57,13 @@ describe('AppController (e2e)', () => {
     it('GET 200', () => {
       return request(app.getHttpServer()).get('/movies/1').expect(200);
     });
-    it.todo('DELETE');
-    it.todo('DELETE');
+
+    it('PATCH 200', () => {
+      return request(app.getHttpServer()).patch('/movies/1').expect(200);
+    });
+
+    it('DELETE 200', () => {
+      return request(app.getHttpServer()).delete('/movies/1').expect(200);
+    });
   });
 });
